ab7e07a53d3a4991dbebdb086ad2e5082731d7e2
Seo98 <tjtmdvy8271@naver.com>
2025-11-16 11:25:11 +0900

    c# Setting

---
+++ backJoon_C#.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        int a1 = int.Parse(Console.ReadLine());
+        int a2 = int.Parse(Console.ReadLine());
+        int a3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(answer);
+    }
+}
